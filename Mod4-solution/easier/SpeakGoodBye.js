71c46387f6c6a476c65e15479391e0f61faf8c57
tgmurombo <murombot000@gmail.com>
2022-07-23 06:35:48 -0700

    Modmod4

--- Mod4-solution/easier/SpeakGoodBye.js
+++ Mod4-solution/easier/SpeakGoodBye.js
@@ -1,8 +1,20 @@
 // NOTE! The steps in this file are basically identical to the ones you
+// NOTE! The steps in this file are basically identical to the ones you
 // performed in the SpeakHello.js file.
 
 // STEP 6: Wrap the entire contents of SpeakGoodBye.js inside of an IIFE
 // See Lecture 52, part 2
+
+(function (window) {
+  var byeSpeaker = {};
+  var speakWord = "Good Bye";
+  function speak(name) {
+    console.log(speakWord + " " + name);
+  }
+  byeSpeaker.speak = speak;
+  window.byeSpeaker = byeSpeaker;
+
+})(window)
 
 
 // STEP 7: Create an object, called 'byeSpeaker' to which you will attach
@@ -11,16 +23,15 @@
 // var byeSpeaker =
 
 // DO NOT attach the speakWord variable to the 'byeSpeaker' object.
-// var speakWord = "Good Bye";
+var speakWord = "Good Bye";
 
-// // STEP 8: Rewrite the 'speak' function such that it is attached to the
-// // byeSpeaker object instead of being a standalone function.
-// // See Lecture 52, part 2
-// function speak(name) {
-//   console.log(speakWord + " " + name);
-// }
+// STEP 8: Rewrite the 'speak' function such that it is attached to the
+// byeSpeaker object instead of being a standalone function.
+// See Lecture 52, part 2
+function speak(name) {
+  console.log(speakWord + " " + name);
+}
 
-// // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
-// // 'byeSpeaker' on the global scope as well.
-// // xxxx.xxxx = byeSpeaker;
- 
+// STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
+// 'byeSpeaker' on the global scope as well.
+// xxxx.xxxx = byeSpeaker;
